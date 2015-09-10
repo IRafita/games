d8d00bb077eee56bc909d05eb5a93088b4323c79
ARNAU SISTACH REINOSO <asistare7.alumnes@ub052229.mat.ub.edu>
2015-09-10 12:06:09 +0200

    en progres, gairebe acabat el intent 001

---
+++ http_juegosdelogica.net/Estrategia/16Fitchas/c/intent-001/lili.c
@@ -0,0 +1,74 @@
+
+/*
+Llegenda
+	SA	Struct active
+	SPF	Struct pasive first
+	SPE	Struct pasive end
+	ST	Struct temporal
+	iM	i for the mutations
+	shA	short int active
+	shT	short int temporal
+	lM	list of mutation
+	lA	list All
+
+	newS	Funcio per a crear una nova structura
+*/
+
+
+
+int main (void)
+{
+	unsigned int level = 0, iM, contador = 0;
+	struct llista * SA, * SPFirst, * SPEnd;
+	unsigned short int shA
+
+
+	do
+	{
+		SA = SPFirst;
+/* Normalment aquesta sobraria per la versio del futur */
+		SPFirst = NULL;
+		level++;
+
+/* El codi el intento fer el mes proxim al dibuix */
+		do
+		{
+			shA = SA->element;
+
+			for (iM = 0; iM < sizeMutation; iM++)
+			{
+				shT = shA ^ lM [ iM ];
+				if ( shT )
+					if ( ! lA [ shT ] )
+					{
+						lA [ shT ] = shA;
+						contador++;
+
+/* Add SP, aqui es el punt clau per afegir o no el codi del futur */
+				//		SPF = newS ( shT, contador ); /* canvi, ja que se repetie en els dos cassos */ he detectat que no funciona com volia ;)
+						if (SPFirst)
+						{
+/* Seria facilment millorable, ja que un va seguit de l'altre, no necessitariem estructures amb punters ' sobren les estructures ' */
+							ST = SPEnd;
+							SPEnd = newS ( shT, contador );
+							ST->next = SPEnd;
+						} else
+						{
+							SPEnd = newS ( shT, contador );
+							SPFirst = SPEnd;
+						}
+					}
+				else
+				{
+					printf ("Finish game\n");
+					exit (0);
+				}
+			}
+
+			SA = SA->next;
+		} while ( SA );
+	} while ( SPFirst );
+
+printf ("No ha trobat solucio, el més probable es que no existeixi\n");
+return 0;
+}
